fb6b81f22ad4b66cf7fb716ce46986f27ef7ee28
Jesse Bowden <ariabowden@Jesses-MacBook-Air.local>
2026-01-15 12:44:33 -0500

    Initial again

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { EveShell } from './EveShell';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: <EveShell />,
   },
 ]);
 
